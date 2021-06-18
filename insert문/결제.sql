a90c9c69818644bc8dd1fd1429707c883d6b1e78
jduckling1024 <jduck1024@naver.com>
2021-06-19 01:23:21 +0900

    [수정] jar 파일 정리

--- insert문/결제.sql
+++
@@ -1,8 +0,0 @@
-/*
--- Query: SELECT * FROM rgb.payment
-LIMIT 0, 1000
-
--- Date: 2020-11-11 23:59
-*/
-INSERT INTO `` (`id`,`amountSum`,`paidAmount`,`cancellationStatus`,`progress`) VALUES (1,1000000,0,0,'101');
-INSERT INTO `` (`id`,`amountSum`,`paidAmount`,`cancellationStatus`,`progress`) VALUES (2,2000000,2000000,0,'102');
--- insert문/주문.sql
+++
@@ -1,8 +0,0 @@
-/*
--- Query: SELECT * FROM rgb.order
-LIMIT 0, 1000
-
--- Date: 2020-11-11 23:59
-*/
-INSERT INTO `` (`id`,`productId`,`userId`,`amount`,`purchaseDate`) VALUES (1,8,'jduck1024',2,'2020-10-24 09:18:00');
-INSERT INTO `` (`id`,`productId`,`userId`,`amount`,`purchaseDate`) VALUES (2,9,'jduck1024',3,'2020-10-24 10:24:00');
